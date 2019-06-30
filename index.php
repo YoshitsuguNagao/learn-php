3c9f85de2eb534af757c1e86c4a970b464fbf33d
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-06-30 21:40:10 +0200

    Conditional Statements done

--- index.php
+++ index.php
@@ -145,6 +145,32 @@
   //echo true == 1;
   //echo false == "";
   //echo true === 1;
+
+  // #11 Conditional statements
+  // $price = 20;
+  // if($price < 30){
+  //   echo 'if condition met';
+  // } elseif($price === 20) {
+  //   echo 'elseif condition met';
+  // } else {
+  //   echo 'condition not met';
+  // }
+  $products = [
+    ['name' => 'shiny star', 'price' => 20],
+    ['name' => 'green shell', 'price' => 10],
+    ['name' => 'red shell', 'price' => 15],
+    ['name' => 'gold coin', 'price' => 5],
+    ['name' => 'lightning bolt', 'price' => 40],
+    ['name' => 'banana skin', 'price' => 2]
+  ];
+  foreach($products as $product){
+    // if($product['price'] < 15 && $product['price'] > 2){
+    //   echo $product['name'] . '<br />';
+    // }
+    // if($product['price'] > 20 || $product['price'] < 10){
+    //   echo $product['name'] . '<br />';
+    // }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
