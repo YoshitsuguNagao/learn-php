8c19d2b15b74aa90b4fe582fc368da718610600c
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-02 21:30:12 +0200

    class done

--- sandbox.php
+++ sandbox.php
@@ -48,6 +48,7 @@
   // // make directory
   // mkdir('quotes');
 
+  // #40 file system
   // $file = 'quotes.txt';
   // // opening a file for reading
   // $handle = fopen($file, 'r');
@@ -66,6 +67,50 @@
   // fwrite($handle, "\nEverything popular is wrong.");
   // fclose($file);
   // unlink($file);
+
+  // #41,42 Class
+  class User {
+    // public $email;
+    // public $name;
+    private $email;
+    private $name;
+    public function __construct($name, $email){
+      // $this->name = 'mario';
+      // $this->email = 'mario@thenetninja.co.uk';
+      $this->name = $name;
+      $this->email = $email;
+    }
+    public function login(){
+      // echo 'the user logged in';
+      echo $this->name . ' logged in';
+    }
+    public function getName(){
+      return $this->name;
+    }
+    public function setName($name){
+      if(is_string($name) && strlen($name) > 1){
+        $this->name = $name;
+        return "name updated to $name";
+      } else {
+        return 'not a valid name';
+      }
+    }
+  }
+  // $userOne = new User();
+
+  // $userOne->login();
+  // echo $userOne->name;
+
+  $userTwo = new User('yoshi', 'yoshi@thenetninja.co.uk');
+  // echo $userTwo->email;
+  // $userTwo->login();
+  // $userTwo->name = 'mario';
+  // echo $userTwo->name;
+  // echo $userTwo->getName();
+  // echo $userTwo->setName('shaun');
+  // echo $userTwo->getName();
+
+
 ?>
 
 <!DOCTYPE html>
