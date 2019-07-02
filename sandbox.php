f024ca62aeab3022f5361f5050bcde81e6121349
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-02 16:35:00 +0200

    Superglobals done

--- sandbox.php
+++ sandbox.php
@@ -9,6 +9,13 @@
   // }
   // echo $score > 40 ? 'high score!' : 'low score!';
 
+  // #35 superglobals
+  //$_GET['name'], $_POST['name']
+  echo $_SERVER['SERVER_NAME'] . '<br />';
+  echo $_SERVER['REQUEST_METHOD'] . '<br />';
+  echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+  echo $_SERVER['PHP_SELF'] . '<br />';
+
 ?>
 
 <!DOCTYPE html>
